909bb7ee39dbb124d2e9b3573b7748132dfa915a
Simon <simon.faksvag@gmail.com>
2024-08-28 17:14:09 +0200

    first svelte project

---
+++ src/App.svelte
@@ -0,0 +1,62 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import "./app.css";
+  let name = 'Peasant'
+  let beltcolour = 'black'
+
+  const handleClick = () => {
+    beltcolour = 'pink'
+  }
+
+  const handleInput = (e) => {
+   beltcolour = e.target.value
+  }
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Well hello there {name}!</h1>
+<p style="color: {beltcolour}">{beltcolour} belt</p>
+<button on:click={handleClick}>update belt colour</button>
+<!-- <input type="text" on:input={handleInput} value={beltcolour}> -->
+<input type="text" bind:value={beltcolour}>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
